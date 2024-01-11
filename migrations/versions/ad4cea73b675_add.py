7a4e366e00dfd054ce2267b280ff62312ed1f8f7
abdirahmanabdullahiissa <abdirahman.abdullahi@student.moringaschool.com>
2024-01-11 14:40:48 +0300

    done

---
+++ migrations/versions/ad4cea73b675_add.py
@@ -0,0 +1,24 @@
+"""add
+
+Revision ID: ad4cea73b675
+Revises: 
+Create Date: 2024-01-11 14:38:09.162819
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ad4cea73b675'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ model.py
@@ -0,0 +1,73 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+engine = create_engine("sqlite:///restaurants.db") 
+Base.metadata.create_all(engine) 
+Session = sessionmaker(bind=engine)
+session = Session() 
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
+    price = Column(Integer())
+
+#relationships 
+    reviews = relationship('Review', back_populates= 'restaurant')
+    customers = association_proxy('reviews', 'customer', creator=lambda cs: Review(customer=cs))
+
+    def __repr__(self):
+        return f"Restaurant(id={self.id}, name={self.name}, price={self.price})"
+    
+    def reviews_for_restaurant(self):
+        return self.reviews
+
+    def customer_who_reviewed(self):
+        return [review.customer for review in self.reviews]
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
+
+#relationships 
+    reviews = relationship('Review', back_populates='customer')
+    restaurants = restaurants = association_proxy('reviews', 'restaurant', creator=lambda rs: Review(restaurant=rs))
+
+    def __repr__(self):
+        return f'Customer(id={self.id}, first_name={self.first_name}, last_name={self.last_name})'
+    
+    def reviews_by_customer(self):
+        return self.reviews
+
+    def restaurant_by_customer(self):
+        return [review.restaurant for review in self.reviews]
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    star_rating = Column(Integer())
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+
+#relationships 
+    restaurant = relationship('Restaurant', back_populates='reviews')
+    customer = relationship('Customer', back_populates='reviews')
+
+    def __repr__(self):
+        return f'Review(id={self.id}, comments={self.comments}, star_rating={self.star_rating})'
+    
+    def customer_reviewed_this(self):
+        return self.customer
+
+    def restaurant_for_review(self):
+        return self.restaurant
